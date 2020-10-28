f795594cf696f7c64ab53888012e5c0815b04fa7
Lukas Forst <lukas@forst.pw>
2020-10-28 01:03:21 +0100

    use just binary stuff

--- rabin/cryptosystem/file.py
+++ rabin/cryptosystem/file.py
@@ -1,4 +1,4 @@
-from typing import Optional, TextIO
+from typing import Optional, BinaryIO
 
 from rabin.crypto_configuration import BLOCK_SIZE_BYTES
 from rabin.cryptosystem.base import RabinCryptosystem
@@ -18,10 +18,11 @@ class FileRabinCryptosystem(RabinCryptosystem):
         """
         encrypted_file_path = plaintext_file + '.enc'
         with open(plaintext_file, 'rb') as plaintext_file:
-            with open(encrypted_file_path, 'w') as encrypted_file:
+            with open(encrypted_file_path, 'wb') as encrypted_file:
                 # write padding to the encrypted file as a header
                 if type(self._ps) == NonceBitsStrategy:
-                    encrypted_file.write(f'{self._ps.nonce}\n')
+                    encrypted_file.write(self._int_to_bytes(self._ps.nonce))
+                    encrypted_file.write(b'\n')
 
                 cs = IntegerRabinCryptosystem(self._ps)
 
@@ -31,8 +32,8 @@ class FileRabinCryptosystem(RabinCryptosystem):
                     plaintext = self._bytes_to_int(plaintext_bytes)
                     # encrypt the int
                     ciphertext = cs.encrypt(pk, plaintext)
-                    # write it as plain int
-                    encrypted_file.write(f'{ciphertext}\n')
+                    # write it in bytes to the file
+                    encrypted_file.write(self._int_to_bytes(ciphertext))
                     # read next bytes
                     plaintext_bytes = plaintext_file.read(BLOCK_SIZE_BYTES)
 
@@ -43,7 +44,7 @@ class FileRabinCryptosystem(RabinCryptosystem):
         Decrypts the given encrypted file, returns decrypted file path.
         """
         decrypted_file_path = ciphertext_file + '.dec'
-        with open(ciphertext_file, 'r') as encrypted_file:
+        with open(ciphertext_file, 'rb') as encrypted_file:
             with open(decrypted_file_path, 'wb') as decrypted_file:
                 # read header and initialize nonce
                 if type(self._ps) == NonceBitsStrategy:
@@ -51,15 +52,31 @@ class FileRabinCryptosystem(RabinCryptosystem):
 
                 cs = IntegerRabinCryptosystem(self._ps)
 
-                for ciphertext in encrypted_file.readlines():
+                ciphertext_bytes = encrypted_file.read(384)
+                while ciphertext_bytes:
+                    ciphertext = self._bytes_to_int(ciphertext_bytes)
                     # decrypt block by block
-                    plaintext = cs.decrypt(sk, int(ciphertext))
+                    plaintext = cs.decrypt(sk, ciphertext)
                     # convert integer to bytes
                     plaintext_bytes = self._int_to_bytes(plaintext)
                     # write the bytes
                     decrypted_file.write(plaintext_bytes)
+                    # read next bytes
+                    ciphertext_bytes = encrypted_file.read(384)
 
         return decrypted_file_path
+
+    def _initialize_nonce(self, ciphertext_file: BinaryIO):
+        nonce_bytes = b''
+        # read first line from the file where the nonce is
+        byte = ciphertext_file.read(1)
+        while byte and byte != b'\n':
+            nonce_bytes += byte
+            byte = ciphertext_file.read(1)
+        # convert nonce to int
+        nonce = self._bytes_to_int(nonce_bytes)
+        # initialise the nonce strategy
+        self._ps = NonceBitsStrategy(nonce)
 
     @staticmethod
     def _int_to_bytes(integer: int) -> bytes:
@@ -68,7 +85,3 @@ class FileRabinCryptosystem(RabinCryptosystem):
     @staticmethod
     def _bytes_to_int(bts: bytes) -> int:
         return int.from_bytes(bts, 'big')
-
-    def _initialize_nonce(self, ciphertext_file: TextIO):
-        nonce = int(ciphertext_file.readline())
-        self._ps = NonceBitsStrategy(nonce)
